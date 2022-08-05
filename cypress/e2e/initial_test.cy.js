444e16f16d1e3840874ebf5cccf4f84d4d322fe2
bruno <brunoomf1@gmail.com>
2022-08-05 15:02:48 -0300

    add new tests

--- cypress/e2e/initial_test.cy.js
+++ cypress/e2e/initial_test.cy.js
@@ -1,30 +1,53 @@
 import Requests from "../Requests/Requests"
 import groupsData from "../fixtures/Groups/groupsData.json"
 import requestStatus from "../fixtures/Groups/groupRequestStatus.json"
+
+
+
 describe('Criando grupos', () => {
 
   it('Criar Grupos com sucesso',function() {
-    var status = requestStatus.createGroup.Successful
+    const groupData = groupsData.CorrectData
 
-    groupsData.forEach(function(group){
+    groupData.forEach(function(group){
+      Requests.cleanGroups(group.GroupName) 
+    })
+    
+    const status = requestStatus.createGroup.Successful
+
+    groupData.forEach(function(group){
       Requests.createGroupRequest(group.GroupName,status.code,status.message)
     })
     
   })
 
-  it('Não é possivel criar grupos que já existem', function() {
-    var status = requestStatus.createGroup.Bad
-    groupsData.forEach(function(group){
+  it('[+]Não é possivel criar grupos que já existem', function() {
+
+    const status = requestStatus.createGroup.AlredyExists
+    const groupData = groupsData.CorrectData
+
+    groupData.forEach(function(group){
       Requests.createGroupRequest(group.GroupName,status.code,status.message)
     })
 
+    groupData.forEach(function(group){
+      Requests.cleanGroups(group.GroupName) 
+    })
+
+  })
+
+  it('[-] Não deve ser possivel criar grupos utilizando dados de tipo não String',function() {
+    const status = requestStatus.createGroup.Validation
+    const groupData = groupsDatagit.WrongData
+
+    groupData.forEach(function(group){
+      Requests.createGroupRequest(group.GroupName,status.code,status.message)
+    })
+
+    groupData.forEach(function(group){
+      Requests.cleanGroups(group.GroupName) 
+    })
+    
   })
   
-})
-
-after(function(){
-  var groups = groupsData
-  groups.forEach(function(group){
-    Requests.cleanGroups(group.GroupName) 
-  })
 })
